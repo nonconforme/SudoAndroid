f2e5748ecc7a108a6a37b923b8b4daf0ab3e7b82
alkurop <alkurop@gmail.com>
2015-04-20 09:44:05 +0300

    - converted Contacts model to Parcelable

--- app/src/main/java/com/thinkmobiles/sudo/models/Contacts.java
+++ app/src/main/java/com/thinkmobiles/sudo/models/Contacts.java
@@ -1,11 +1,81 @@
 package com.thinkmobiles.sudo.models;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by omar on 19.04.15.
  */
-public class Contacts {
+public class Contacts implements Parcelable {
+
+    private String firstName;
+    private String lastName;
+    private String avatarUrl;
+
+    private String phoneNumber1;
+    private String phoneNumber2;
+    private String phoneNumber3;
+
+
+    public Contacts() {
+    }
+
+    public Contacts(
+            String firstName,
+            String lastName,
+            String avatarUrl,
+            String phoneNumber1,
+            String phoneNumber2,
+            String phoneNumber3) {
+        this. firstName = firstName;
+        this. lastName = lastName;
+        this. avatarUrl = avatarUrl;
+        this. phoneNumber1 = phoneNumber1;
+        this. phoneNumber2 = phoneNumber2;
+        this. phoneNumber3 = phoneNumber3;
+    }
+
+    public Contacts(Parcel in) {
+
+        String[] data = new String[3];
+
+        in.readStringArray(data);
+        this.firstName = data[0];
+        this.lastName = data[1];
+        this.avatarUrl = data[2];
+
+        this.phoneNumber1 = data[3];
+        this.phoneNumber2 = data[4];
+        this.phoneNumber3 = data[5];
+    }
+
+
+    @Override
+    public int describeContents() {
+
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeStringArray(new String[]{firstName, lastName, avatarUrl, phoneNumber1, phoneNumber2, phoneNumber3});
+
+    }
+
+
+    public static final Parcelable.Creator<Contacts> CREATOR = new Parcelable.Creator<Contacts>() {
+        public Contacts createFromParcel(Parcel in) {
+            return new Contacts(in);
+        }
+
+        public Contacts[] newArray(int size) {
+            return new Contacts[size];
+        }
+    };
+
+
     public String getFirstName() {
         return firstName;
     }
@@ -53,17 +123,6 @@ public class Contacts {
     public void setPhoneNumber3(String phoneNumber3) {
         this.phoneNumber3 = phoneNumber3;
     }
-
-    private String firstName;
-    private String lastName;
-    private String  avatarUrl;
-
-    private String phoneNumber1;
-    private String  phoneNumber2;
-    private String  phoneNumber3;
-
-
-
 
 
 }
