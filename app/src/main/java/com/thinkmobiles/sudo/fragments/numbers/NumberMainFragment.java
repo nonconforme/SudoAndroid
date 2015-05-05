2667da83b7f58201d1900b9d35a6cf2cd48cadeb
alkurop <alkurop@gmail.com>
2015-05-05 13:12:21 +0300

    - editit list_item_number.xml

--- app/src/main/java/com/thinkmobiles/sudo/fragments/numbers/NumberMainFragment.java
+++ app/src/main/java/com/thinkmobiles/sudo/fragments/numbers/NumberMainFragment.java
@@ -1,16 +1,10 @@
 package com.thinkmobiles.sudo.fragments.numbers;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.thinkmobiles.sudo.Main_Activity;
 import com.thinkmobiles.sudo.R;
-import com.thinkmobiles.sudo.ToolbarManager;
-import com.thinkmobiles.sudo.fragments.numbers.BaseNumbersFragment;
 
 /**
  * Created by njakawaii on 29.04.2015.
