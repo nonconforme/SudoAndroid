e0e2558865a9ccd024c02f25243ae844e781b91b
thinkmobiles <github@thinkmobiles.com>
2015-05-07 13:38:30 +0300

    1. Updated lastChats api

--- app/src/main/java/com/thinkmobiles/sudo/models/chat/LastChatsModel.java
+++ app/src/main/java/com/thinkmobiles/sudo/models/chat/LastChatsModel.java
@@ -6,13 +6,13 @@ import java.util.List;
  * Created by njakawaii on 07.05.2015.
  */
 public class LastChatsModel {
-    public List<MessageModel> getLastmessage() {
+    public MessageModel getLastmessage() {
         return lastmessage;
     }
 
-    public void setLastmessage(List<MessageModel> lastmessage) {
+    public void setLastmessage(MessageModel lastmessage) {
         this.lastmessage = lastmessage;
     }
 
-    private List<MessageModel> lastmessage;
+    private MessageModel lastmessage;
 }
