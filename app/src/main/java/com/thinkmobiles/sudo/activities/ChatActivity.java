ede08ac8124b7a7ff88dd7b2c76bf1ef0798cf59
thinkmobiles <github@thinkmobiles.com>
2015-05-08 11:21:04 +0300

    1. Added socked receiver.

--- app/src/main/java/com/thinkmobiles/sudo/activities/ChatActivity.java
+++ app/src/main/java/com/thinkmobiles/sudo/activities/ChatActivity.java
@@ -5,14 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.google.gson.Gson;
 import com.thinkmobiles.sudo.R;
 import com.thinkmobiles.sudo.ToolbarManager;
 import com.thinkmobiles.sudo.adapters.ChatListAdapter;
+import com.thinkmobiles.sudo.core.APIConstants;
 import com.thinkmobiles.sudo.core.rest.RetrofitAdapter;
 import com.thinkmobiles.sudo.global.App;
 import com.thinkmobiles.sudo.global.Constants;
@@ -21,6 +29,10 @@ import com.thinkmobiles.sudo.models.chat.MessageModel;
 import com.thinkmobiles.sudo.utils.ContactManager;
 import com.thinkmobiles.sudo.utils.Utils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 import java.util.List;
 
 import retrofit.Callback;
@@ -43,6 +55,14 @@ public class ChatActivity extends ActionBarActivity {
     private Toolbar toolbar;
     private String mOwnerNumber;
     private String mCompanionNumber;
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket(APIConstants.SERVER_URL);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     public static final String CHAT_MODEL = "chat";
@@ -58,11 +78,55 @@ public class ChatActivity extends ActionBarActivity {
         initGetMessageCB();
         initSendMessageCB();
         getMessages();
-        this.overridePendingTransition(R.anim.anim_edit_profile_slide_in, R.anim.anim_view_profile_slide_out);
+        initSocked();
+         this.overridePendingTransition(R.anim.anim_edit_profile_slide_in, R.anim.anim_view_profile_slide_out);
         ToolbarManager.getInstance(this).changeToolbarTitleAndIcon("Chat", R.drawable.ic_launcher);
 
-
+        mSocket.connect();
     }
+
+    private void initSocked() {
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+        mSocket.on("receiveMessage", onReceive);
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("uId",  App.getuId());
+            mSocket.emit("authorize",jsonObject);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Emitter.Listener onReceive = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            JSONObject data = (JSONObject) args[0];
+            Gson gson = new Gson();
+            MessageModel message = gson.fromJson(data.toString(), MessageModel.class);
+            Log.d("socket", "received");
+            //TODO add to list new message
+
+        }
+    };
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mSocket.off("receiveMessage", onReceive);
+        mSocket.disconnect();
+    }
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+
+                    Toast.makeText(getApplicationContext(),
+                            "Connection problims ", Toast.LENGTH_LONG).show();
+
+        }
+    };
 
     private void initSendMessageCB() {
         mSendMessageCB = new Callback<DefaultResponseModel>() {
