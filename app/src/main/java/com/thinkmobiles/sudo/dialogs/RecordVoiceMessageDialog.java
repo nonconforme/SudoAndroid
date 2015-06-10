9a9eabdd0b8d49804cdc6514afc58a63eb741cfe
alkurop <alkurop@gmail.com>
2015-06-10 11:56:25 +0300

    - voice message recorder view

--- app/src/main/java/com/thinkmobiles/sudo/dialogs/RecordVoiceMessageDialog.java
+++ app/src/main/java/com/thinkmobiles/sudo/dialogs/RecordVoiceMessageDialog.java
@@ -1,15 +1,20 @@
 package com.thinkmobiles.sudo.dialogs;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.thinkmobiles.sudo.R;
 import com.thinkmobiles.sudo.audio.VoiceRecorder;
 import com.thinkmobiles.sudo.global.Constants;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by omar on 03.06.15.
@@ -17,7 +22,7 @@ import com.thinkmobiles.sudo.global.Constants;
 public class RecordVoiceMessageDialog extends DialogFragment implements View.OnClickListener {
 
 
-    public interface  RecordVoiceMessageDialogCallback{
+    public interface RecordVoiceMessageDialogCallback {
         void onSendVoiceMessage();
     }
 
@@ -26,6 +31,12 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
     private VoiceRecorder mVoiceRecorder;
     private boolean recording = false;
     private RecordVoiceMessageDialogCallback cb;
+    private boolean hasRecording = false;
+    private TextView tvTitleRD;
+    private TextView tvTextRD;
+    private ProgressBar pbDR;
+    private AsyncTask<Void, Integer, Void> mPreRecordCounter;
+    private AsyncTask<Void, Integer, Void> mRecordCounter;
 
 
     private ImageView ivDialogRecord_CF, ivDialogSend_CF;
@@ -44,6 +55,7 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         filePath = getArguments().getString(Constants.FILEPATH);
+
     }
 
     @Override
@@ -51,6 +63,7 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
         View v = inflater.inflate(R.layout.dialog_record_voice, container, false);
         findUI(v);
         setListeners();
+        initAsyncTasks();
         return v;
     }
 
@@ -58,13 +71,15 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         this.activity = activity;
-        cb = (RecordVoiceMessageDialogCallback)activity;
+        cb = (RecordVoiceMessageDialogCallback) activity;
     }
 
     private void findUI(View v) {
         ivDialogRecord_CF = (ImageView) v.findViewById(R.id.ivDialogRecord_CF);
         ivDialogSend_CF = (ImageView) v.findViewById(R.id.ivDialogSend_CF);
-
+        tvTitleRD = (TextView) v.findViewById(R.id.tvTitle_RD);
+        tvTextRD = (TextView) v.findViewById(R.id.tvText_RD);
+        pbDR = (ProgressBar) v.findViewById(R.id.pb_DR);
     }
 
     @Override
@@ -73,11 +88,11 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
             case R.id.ivDialogRecord_CF:
                 if (!recording) {
                     setupVoiceRecorder();
-                    mVoiceRecorder.recordStart();
+                    mPreRecordCounter.execute();
                     ivDialogRecord_CF.setImageResource(android.R.drawable.ic_menu_close_clear_cancel);
                     recording = true;
-                }
-                else{
+                    hasRecording = true;
+                } else {
                     mVoiceRecorder.recordStop();
 
                     dismiss();
@@ -85,8 +100,10 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
                 break;
 
             case R.id.ivDialogSend_CF:
-                mVoiceRecorder.recordStop();
-                cb.onSendVoiceMessage();
+                if (hasRecording) {
+                    mVoiceRecorder.recordStop();
+                    cb.onSendVoiceMessage();
+                }
                 dismiss();
                 break;
 
@@ -105,7 +122,76 @@ public class RecordVoiceMessageDialog extends DialogFragment implements View.OnC
         mVoiceRecorder = new VoiceRecorder(filePath);
     }
 
+    private void initAsyncTasks() {
+        mPreRecordCounter = new AsyncTask<Void, Integer, Void>() {
+            int counter = 3;
 
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                while (counter > 0) {
+                    publishProgress(counter);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    counter--;
+
+                }
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                tvTextRD.setText(activity.getString(R.string.recording_starts) + " " + String.valueOf(values[0]) + " " +
+                        activity.getString(R.string.seconds));
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                mRecordCounter.execute();
+            }
+        };
+
+
+        mRecordCounter = new AsyncTask<Void, Integer, Void>() {
+            int counter = 0;
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                mVoiceRecorder.recordStart();
+                while (counter < 31) {
+                    publishProgress(counter);
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    counter++;
+
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+
+                tvTextRD.setText(activity.getString(R.string.recording) + " " + String.valueOf(values[0]) + " " +
+                        activity
+                        .getString(R.string.seconds));
+                pbDR.setProgress(values[0]);
+                pbDR.setMax(30);
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                mVoiceRecorder.recordStop();
+                tvTextRD.setText(activity.getString(R.string.recorded));
+            }
+        };
+    }
 
 }
 
